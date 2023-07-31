2d39f32bf2cdec0a4f1372621ae5deeae27faae1
StuartMolnar <stuartmolnar@hotmail.com>
2023-07-31 00:32:10 -0700

    Add web scraping functionality for League of Legends ranks

---
+++ league_data/app.ts
@@ -0,0 +1,135 @@
+import puppeteer, { Page, ElementHandle } from 'puppeteer';
+import fs from 'fs';
+import yaml from 'js-yaml';
+import logger from './log_conf';
+import RankDataProcessor from './process_data';
+import util from 'util';
+
+/**
+ * Represents the configuration for the scraper, defined in the YAML config file.
+ * URL - The URL to scrape.
+ * SELECTOR - The CSS selector to find the elements to scrape.
+ * HEADER - The header text that should be ignored in the scraped data.
+ */
+interface Config {
+    URL: string;
+    SELECTOR: string;
+    HEADER: string;
+}
+
+let config: Config;
+
+try {
+    const configFile = fs.readFileSync('app_conf.yml', 'utf8');
+    config = yaml.load(configFile) as Config;
+    logger.info(`Configuration loaded from app_conf.yml`);
+} catch (error) {
+    if (error instanceof Error) {
+        logger.error(`Failed to load configuration from app_conf.yml: ${error.message}`);
+    } else {
+        // This will handle situations where the thrown error isn't an instance of Error.
+        logger.error(`Failed to load configuration from app_conf.yml: ${error}`);
+    }
+    throw error;
+}
+
+/**
+ * Extracts the text content from each DOM element that matches the provided CSS selector.
+ * 
+ * @param page - The Puppeteer page instance to run the selector on.
+ * @param selector - The CSS selector to use for finding elements.
+ * 
+ * @returns An array of strings, each representing the text content of a matched element.
+ */
+async function extractDataFromElement(page: Page, selector: string): Promise<string[]> {
+    logger.info(`Extracting text from selector ${selector}`);
+    
+    let elements: ElementHandle[];
+    try {
+        elements = await page.$$(selector);
+    } catch (error) {
+        if (error instanceof Error) {
+            logger.error(`Failed to select elements using the selector "${selector}": ${error.message}`);
+        } else {
+            // This will handle situations where the thrown error isn't an instance of Error.
+            logger.error(`Failed to select elements using the selector "${selector}": ${error}`);
+        }
+        throw error;
+    }
+
+    const texts: string[] = [];
+    for (const element of elements) {
+        let textContent: string;
+        try {
+            textContent = await element.evaluate((node: Element) => node.textContent ? node.textContent.trim() : '');
+        } catch (error) {
+            if (error instanceof Error) {
+                logger.error(`Failed to evaluate text content of an element: ${error.message}`);
+            } else {
+                // This will handle situations where the thrown error isn't an instance of Error.
+                logger.error(`Failed to evaluate text content of an element: ${error}`);
+            }
+            continue; // If we fail to get the text from one element, we skip it and continue with the next
+        }
+        
+        texts.push(textContent);
+    }
+
+    if (texts.length === 0) {
+        logger.error(`No elements found for selector "${selector}" or failed to extract text from them.`);
+    }
+    
+    return texts;
+}
+
+
+
+/**
+ * Scrapes a webpage at the provided URL, using the specified CSS selector to find elements. Each matching element's
+ * text content is processed as a line of data representing a league rank and associated percentage.
+ * 
+ * @param url - The URL of the webpage to scrape.
+ * @param selector - The CSS selector to use for finding elements.
+ * 
+ * @returns A RankDataProcessor instance containing the processed rank data, or null if an error occurred during scraping.
+ */
+async function scrapeUrl(url: string, selector: string): Promise<RankDataProcessor | null> {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    try {
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        let textData = await extractDataFromElement(page, selector);
+        textData = textData.map(item => item.replace(/\s\s+/g, ' ').replace(/\n/g, ' '));
+        if (textData.length !== 0) {
+            if (textData[0] === config.HEADER) {
+                textData.shift();
+            }
+            const processedData = new RankDataProcessor(textData);
+            return processedData;
+        } else {
+            logger.warn(`No data extracted from page. Check your CSS selector and ensure the page structure hasn't changed.`);
+        }
+    } catch (err) {
+        logger.error(`Error scraping URL ${url}: ${err}`);
+    } finally {
+        await browser.close();
+        logger.info(`Browser closed after scraping URL ${url}`);
+    }
+
+    return null;
+}
+
+
+scrapeUrl(config.URL, config.SELECTOR)
+    .then(processedData => {
+        if (processedData) {
+            logger.info(`Scraped data processed successfully`);
+            logger.debug(util.inspect(processedData, { showHidden: false, depth: null }));
+        } else {
+            logger.error("Failed to scrape data");
+        }
+    })
+    .catch(error => {
+        logger.error(`Error occurred during the scrape process: ${error.message}`);
+    });
---
+++ league_data/process_data.ts
@@ -0,0 +1,118 @@
+import { Decimal } from 'decimal.js';
+import logger from './log_conf';
+
+/**
+ * Interface for the rank data object.
+ * 
+ * @interface
+ * 
+ * @property {string} tier - The tier of the league rank.
+ * 
+ * @property {Decimal} rankPercentage - The percentage of players at or above this rank.
+ * 
+ * @property {Decimal} cumulativePercentage - The cumulative percentage of players at or above this rank.
+ */
+interface RankData {
+    tier: string;
+    rankPercentage: Decimal;
+    cumulativePercentage: Decimal;
+}
+
+            
+
+/**
+ * Processes a list of league ranks with associated percentages. Adjusts the percentages (if necessary) so that they sum up to 100.
+ */
+class RankDataProcessor {
+    public processedData: RankData[];
+
+    /**
+     * Creates a new RankDataProcessor instance.
+     *
+     * Processes a list of league ranks with associated percentages. Adjusts the percentages (if necessary) so that they sum up to 100.
+     * 
+     * @param lines - An array of strings, each representing a league rank and associated percentage.
+     * 
+     * @returns A new RankDataProcessor instance, with the processed data returned as its value.
+     * 
+     * @usage const processedData = new RankDataProcessor(lines);
+     */
+    constructor(lines: string[]) {
+        try {
+            this.processedData = this.adjustPercentages(this.parseLines(lines));
+            logger.info("RankDataProcessor instance created successfully.");
+        } catch(error) {
+            if (error instanceof Error) {
+                logger.error(`Error occurred while creating RankDataProcessor instance: ${error.message}`);
+            } else {
+                // This will handle situations where the thrown error isn't an instance of Error.
+                logger.error(`Error occurred while creating RankDataProcessor instance: ${error}`);
+            }
+            throw error;
+        }
+    }
+
+    /**
+     * Parses lines into an array of objects, each containing a tier, rank percentage, and cumulative percentage.
+     *
+     * @param lines - An array of strings, each representing a league rank and associated percentage.
+     * @returns An array of objects, each containing a tier, rank percentage, and cumulative percentage.
+     */
+    private parseLines(lines: string[]): RankData[] {
+        let regex = /^([\D]*)\s(\d+.\d+)%/;
+        let cumulativePercentage = new Decimal(0);
+        let processedData: Array<{tier: string, rankPercentage: Decimal, cumulativePercentage: Decimal}> = [];
+
+        for(let line of lines) {
+            let match = line.match(regex);
+
+            if (match) {
+                let tier = match[1].trim();
+                let rankPercentage = new Decimal(match[2]);
+                cumulativePercentage = cumulativePercentage.plus(rankPercentage);
+                processedData.push({ tier, rankPercentage, cumulativePercentage });
+            } else {
+                logger.error(`Could not parse line: "${line}"`);
+                throw new Error(`Could not parse line: "${line}"`);
+            }
+        }
+
+        logger.info("Lines parsed successfully.");
+        return processedData;
+    }
+
+    /**
+     * Adjusts the percentages in the processed data so that they sum up to 100.
+     *
+     * @param processedData - An array of objects, each containing a tier, rank percentage, and cumulative percentage.
+     * @returns An array of objects, each containing a tier, rank percentage, and cumulative percentage, adjusted so that they sum up to 100.
+     */
+    private adjustPercentages(processedData: RankData[]): RankData[] {
+        try {
+            const totalInaccuracy = new Decimal(100).minus(processedData[processedData.length - 1].cumulativePercentage);
+            let cumulativePercentage = new Decimal(0);
+
+            for (let item of processedData) {
+                item.rankPercentage = item.rankPercentage.plus(item.rankPercentage.dividedBy(100).times(totalInaccuracy));
+                cumulativePercentage = cumulativePercentage.plus(item.rankPercentage);
+                item.cumulativePercentage = cumulativePercentage;
+            }
+
+            logger.info("Percentages adjusted successfully.");
+            return processedData;
+        } catch(error) {
+            
+            if (error instanceof Error) {
+                logger.error(`Error occurred while creating RankDataProcessor instance: ${error.message}`);
+                logger.error(`Error occurred while adjusting percentages: ${error.message}`);
+                throw new Error(`Error occurred while adjusting percentages: ${error.message}`);
+            } else {
+                // This will handle situations where the thrown error isn't an instance of Error.
+                logger.error(`Error occurred while adjusting percentages: ${error}`);
+                throw new Error(`Error occurred while adjusting percentages: ${error}`);
+            }
+        }
+    }
+}
+
+export default RankDataProcessor;
