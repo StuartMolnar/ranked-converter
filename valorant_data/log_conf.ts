fbe239d8b8a88cae1d7339f59679ff0862d21d48
StuartMolnar <stuartmolnar@hotmail.com>
2023-07-31 01:43:47 -0700

    Create Valorant Data service

---
+++ valorant_data/log_conf.ts
@@ -0,0 +1,38 @@
+import winston, { format, transports } from 'winston';
+
+// Create a transport for writing to the file
+const fileTransport = new transports.File({
+  level: 'debug', // Log 'info' and above to file
+  filename: `app.log`,
+  format: format.printf(info => {
+    return `${info.timestamp} - ${info.level.toUpperCase()} - ${info.message}`;
+  }),
+  maxsize: 1048576, // max size of 1MB
+});
+
+// Create a transport for logging to the console
+const consoleTransport = new transports.Console({
+  level: 'info', // Only log 'warn' and above to console
+  format: format.printf(info => {
+    return `${info.timestamp} - ${info.level.toUpperCase()} - ${info.message}`;
+  }),
+});
+
+// Create a logger instance
+const logger = winston.createLogger({
+  level: 'debug', // Global logging level
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss.SSS',
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json(),
+  ),
+  transports: [
+    fileTransport,
+    consoleTransport,
+  ],
+});
+
+export default logger;
